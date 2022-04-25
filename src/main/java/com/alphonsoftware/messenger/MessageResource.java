18d354d1aec84e35ce0dbca5fdeafb81f73012ea
Eknath Kadam <eknathkadam@gmail.com>
2022-04-25 16:35:41 +0530

    JSON support in MediaType and parametrized methods

--- src/main/java/com/alphonsoftware/messenger/MessageResource.java
+++ src/main/java/com/alphonsoftware/messenger/MessageResource.java
@@ -3,24 +3,25 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.PathParam;
 import java.util.*;
-
 
 @Path("/messages")
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
-//	@GET
-//	@Produces(MediaType.TEXT_PLAIN)
-//
-//	public String getMessage() {
-//		return "Hellow World@";
-//	}
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
-	public List<Message> getMessage() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getMessages() {
 		System.out.println("Tests");
 		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Path("/{messageID}")
+	@Produces(MediaType.APPLICATION_XML)
+	public String getMessage(@PathParam("messageID") long message) {
+		return "test";
 	}
 }
